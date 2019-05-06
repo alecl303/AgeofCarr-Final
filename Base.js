a8ec469b20713fdac261c8b5881afb44fef82ba8
dragontang20 <50115383+dragontang20@users.noreply.github.com>
2019-05-06 10:09:41 -0700

    Add files via upload

--- Base.js
+++ Base.js
@@ -1,16 +1,16 @@
 	function Base(game){
 		this.game = game;
-		
+
 		this.elem = document.createElement('canvas');
-		
+
 		$(this.elem)
 			.css({height:200, width:500,zIndex:2,top:600-200,position:'absolute'});
 		this.game.elem.append(this.elem);
 		this.ctx = this.elem.getContext('2d');
-		
+
 		this.moveToRight = function(){
-			$(this.elem).css({left: 1250-parseInt($(this.elem).css('width'))});
-				
+			$(this.elem).css({left: 1499-parseInt($(this.elem).css('width'))});
+
 		}
 //		this.moveToRight();
 		this.draw = function(ctx){
@@ -18,7 +18,7 @@
 			ctx.beginPath();
 			ctx.fillStyle = 'pink';
 			ctx.fillRect(0,0,150,500);
-			
+
 		}
 		this.drawBase = function(ctx){
 			ctx.beginPath();
@@ -34,7 +34,5 @@
 			ctx.lineWidth = 5;
 			//ctx.stroke();
 		}
-	
-		this.draw(this.ctx);
-		this.drawBase(this.ctx);
+
 	}
