d9e4d831b4ce08a1fc0b1152b7f59b7f3b7b7c0f
andrewchung500 <50115267+andrewchung500@users.noreply.github.com>
2019-05-07 00:40:11 -0700

    Add files via upload

--- Control.js
+++ Control.js
@@ -7,12 +7,16 @@ function Control(game){
 		this.rightPlayerControl=(function(){return rightPlayerControl();}());
 		this.leftTurretDisplay=(function(){return leftTurretDisplay();}());
 		this.rightTurretDisplay=(function(){return rightTurretDisplay();}());
+		this.leftGoldDisplay=$("<div/>").css({"height":"20px", "width":"100px", "position":"absolute", "top":"65px","left":"5px"});
+		this.rightGoldDisplay=$("<div/>").css({"height":"20px", "width":"100px","position":"absolute", "top":"65px","left":"840px"});
 		this.draw=function(){
 			var wrapper= $("<div/>");
 			$(wrapper).append(_this.leftPlayerControl);
 			$(wrapper).append(_this.rightPlayerControl);
 			$(wrapper).append(_this.leftTurretDisplay);
 			$(wrapper).append(_this.rightTurretDisplay);
+			$(wrapper).append(_this.leftGoldDisplay);
+			$(wrapper).append(_this.rightGoldDisplay);
 			return wrapper;
 			
 		}
@@ -23,6 +27,16 @@ function Control(game){
 				.attr({"src":"goblinmelee.jpg"})
 
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.leftGoldDisplay.html("Cost: $50");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.leftGoldDisplay.html("");
+					}
+				)
 				.click(
 					function(){
 						_this.game.buyCharacter();
@@ -30,10 +44,30 @@ function Control(game){
 				);
 			var leftCharacterButton2=$("<img>")
 				.attr({"src":"goblinarcher.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.leftGoldDisplay.html("Cost: $100");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.leftGoldDisplay.html("");
+					}
+				);
 			var leftCharacterButton3=$("<img>")
 				.attr({"src":"goblingiant.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"});
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.leftGoldDisplay.html("Cost: $200");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.leftGoldDisplay.html("");
+					}
+				);
 			var leftTurretsBox=$("<img>")
 				.attr({"src":"turret.png"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"310px", "border":"1px solid black"})
@@ -54,6 +88,16 @@ function Control(game){
 			var rightCharacterButton1=$("<img>")
 				.attr({"src":"alienmelee.jpg"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.rightGoldDisplay.html("Cost: $50");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.rightGoldDisplay.html("");
+					}
+				)
 				.click(
 					function(){
 						_this.game.buyCharacter();
@@ -61,10 +105,30 @@ function Control(game){
 				);
 			var rightCharacterButton2=$("<img>")
 				.attr({"src":"alienshooter.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.rightGoldDisplay.html("Cost: $100");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.rightGoldDisplay.html("");
+					}
+				);
 			var rightCharacterButton3=$("<img>")
 				.attr({"src":"alienboss.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"});
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
+				.mouseover(
+					function(){
+						_this.rightGoldDisplay.html("Cost: $200");
+					}
+				)
+				.mouseout(
+					function(){
+						_this.rightGoldDisplay.html("");
+					}
+				);
 			var rightTurretsBox=$("<img>")
 				.attr({"src":"turret.png"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"310px", "border":"1px solid black"})
@@ -85,13 +149,33 @@ function Control(game){
 		var leftTurretButton1=$("<img>")
 			.attr({"src":"goblinturret1.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
+			.mouseover(
+				function(){
+					_this.leftGoldDisplay.html("Cost: $300");
+				}
+			)
+			.mouseout(
+				function(){
+					_this.leftGoldDisplay.html("");
+				}
+			)
 			.click(
 				function(){
 				}
 			);
 		var leftTurretButton2=$("<img>")
 			.attr({"src":"goblinturret2.jpg"})
-			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
+			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
+			.mouseover(
+				function(){
+					_this.leftGoldDisplay.html("Cost: $500");
+				}
+			)
+			.mouseout(
+				function(){
+					_this.leftGoldDisplay.html("");
+				}
+			);
 		var exitButton=$("<img>")
 			.attr({"src":"X.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
@@ -111,13 +195,33 @@ function Control(game){
 		var rightTurretButton1=$("<img>")
 			.attr({"src":"alienturret1.jpg"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
+			.mouseover(
+				function(){
+					_this.rightGoldDisplay.html("Cost: $300");
+				}
+			)
+			.mouseout(
+				function(){
+					_this.rightGoldDisplay.html("");
+				}
+			)
 			.click(
 				function(){
 				}
 			);
 		var rightTurretButton2=$("<img>")
 			.attr({"src":"alienturret2.jpg"})
-			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
+			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
+			.mouseover(
+				function(){
+					_this.rightGoldDisplay.html("Cost: $500");
+				}
+			)
+			.mouseout(
+				function(){
+					_this.rightGoldDisplay.html("");
+				}
+			);
 		var exitButton=$("<img>")
 			.attr({"src":"X.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
