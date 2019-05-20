766b2bfa67f8b5eec38fe28a918db89126fb6a1f
andrewchung500 <50115267+andrewchung500@users.noreply.github.com>
2019-05-19 21:16:20 -0700

    Add files via upload

--- Control.js
+++ Control.js
@@ -22,21 +22,15 @@ function Control(game){
 		}
 	//CREATING--------------------------------------------------------
 		function leftPlayerControl(){
-			var leftPlayerControl=$("<div/>").css({"height":"50px", "width":"400px", "position":"absolute", "top":"10px","left":"5px", "background-color":"lightyellow","border":"2px solid black","z-index":2});
+			var characterCost1=$("<span/>").html("$50").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"18px"});
+			var characterCost2=$("<span/>").html("$100").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"115px"});
+			var characterCost3=$("<span/>").html("$200").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"215px"});
+			var leftPlayerControl=$("<div/>")
+					.css({"height":"63px", "width":"400px", "position":"absolute", "top":"10px","left":"5px", "background-color":"lightyellow","border":"2px solid black","z-index":2});
 			var leftCharacterButton1=$("<img>")
 				.attr({"src":"goblinmelee.jpg"})
-
+				.html("$50")
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.leftGoldDisplay.html("Cost: $50");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.leftGoldDisplay.html("");
-					}
-				)
 				.click(
 					function(){
 						_this.game.buySwordGoblin();
@@ -44,30 +38,10 @@ function Control(game){
 				);
 			var leftCharacterButton2=$("<img>")
 				.attr({"src":"goblinarcher.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.leftGoldDisplay.html("Cost: $100");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.leftGoldDisplay.html("");
-					}
-				);
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
 			var leftCharacterButton3=$("<img>")
 				.attr({"src":"goblingiant.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.leftGoldDisplay.html("Cost: $200");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.leftGoldDisplay.html("");
-					}
-				);
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"});
 			var leftTurretsBox=$("<img>")
 				.attr({"src":"turret.png"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"310px", "border":"1px solid black"})
@@ -78,26 +52,22 @@ function Control(game){
 					}
 				);
 			$(leftPlayerControl).append(leftCharacterButton1);
+			$(leftPlayerControl).append(characterCost1);
 			$(leftPlayerControl).append(leftCharacterButton2);
+			$(leftPlayerControl).append(characterCost2);
 			$(leftPlayerControl).append(leftCharacterButton3);
+			$(leftPlayerControl).append(characterCost3);
 			$(leftPlayerControl).append(leftTurretsBox);
 			return leftPlayerControl;
 	}
 		function rightPlayerControl(){
-			var rightPlayerControl=$("<div/>").css({"height":"50px", "width":"400px", "position":"absolute", "top":"10px","left":"840px", "background-color":"lightyellow","border":"2px solid black","z-index":2});
+			var characterCost1=$("<span/>").html("$50").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"18px"});
+			var characterCost2=$("<span/>").html("$100").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"115px"});
+			var characterCost3=$("<span/>").html("$200").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"215px"});
+			var rightPlayerControl=$("<div/>").css({"height":"63px", "width":"400px", "position":"absolute", "top":"10px","left":"840px", "background-color":"lightyellow","border":"2px solid black","z-index":2});
 			var rightCharacterButton1=$("<img>")
 				.attr({"src":"alienmelee.jpg"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.rightGoldDisplay.html("Cost: $50");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.rightGoldDisplay.html("");
-					}
-				)
 				.click(
 					function(){
 						_this.game.buyGunner();
@@ -105,30 +75,10 @@ function Control(game){
 				);
 			var rightCharacterButton2=$("<img>")
 				.attr({"src":"alienshooter.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.rightGoldDisplay.html("Cost: $100");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.rightGoldDisplay.html("");
-					}
-				);
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
 			var rightCharacterButton3=$("<img>")
 				.attr({"src":"alienboss.jpg"})
-				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
-				.mouseover(
-					function(){
-						_this.rightGoldDisplay.html("Cost: $200");
-					}
-				)
-				.mouseout(
-					function(){
-						_this.rightGoldDisplay.html("");
-					}
-				);
+				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"});
 			var rightTurretsBox=$("<img>")
 				.attr({"src":"turret.png"})
 				.css({"height":"45px", "width":"45px", "position":"absolute", "left":"310px", "border":"1px solid black"})
@@ -139,43 +89,28 @@ function Control(game){
 					}
 				);
 			$(rightPlayerControl).append(rightCharacterButton1);
+			$(rightPlayerControl).append(characterCost1);
 			$(rightPlayerControl).append(rightCharacterButton2);
+			$(rightPlayerControl).append(characterCost2);
 			$(rightPlayerControl).append(rightCharacterButton3);
+			$(rightPlayerControl).append(characterCost3);
 			$(rightPlayerControl).append(rightTurretsBox);
 			return rightPlayerControl;
 	}
 	function leftTurretDisplay(){
-		var leftTurretControl=$("<div/>").css({"height":"50px", "width":"300px", "position":"absolute", "top":"10px","left":"5px", "background-color":"lightblue","border":"2px solid black","visibility":"hidden"});
+			var turretCost1=$("<span/>").html("$400").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"18px"});
+			var turretCost2=$("<span/>").html("$1000").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"115px"});
+		var leftTurretControl=$("<div/>").css({"height":"63px", "width":"300px", "position":"absolute", "top":"10px","left":"5px", "background-color":"lightblue","border":"2px solid black","visibility":"hidden"});
 		var leftTurretButton1=$("<img>")
 			.attr({"src":"goblinturret1.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
-			.mouseover(
-				function(){
-					_this.leftGoldDisplay.html("Cost: $300");
-				}
-			)
-			.mouseout(
-				function(){
-					_this.leftGoldDisplay.html("");
-				}
-			)
 			.click(
 				function(){
 				}
 			);
 		var leftTurretButton2=$("<img>")
 			.attr({"src":"goblinturret2.jpg"})
-			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
-			.mouseover(
-				function(){
-					_this.leftGoldDisplay.html("Cost: $500");
-				}
-			)
-			.mouseout(
-				function(){
-					_this.leftGoldDisplay.html("");
-				}
-			);
+			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
 		var exitButton=$("<img>")
 			.attr({"src":"X.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
@@ -186,12 +121,16 @@ function Control(game){
 				}
 			);
 		$(leftTurretControl).append(leftTurretButton1);
+		$(leftTurretControl).append(turretCost1);
 		$(leftTurretControl).append(leftTurretButton2);
+		$(leftTurretControl).append(turretCost2);
 		$(leftTurretControl).append(exitButton);
 		return leftTurretControl;
 	}
 	function rightTurretDisplay(){
-		var rightTurretControl=$("<div/>").css({"height":"50px", "width":"300px", "position":"absolute", "top":"10px","left":"840px", "background-color":"lightblue","border":"2px solid black","visibility":"hidden"});
+			var turretCost1=$("<span/>").html("$400").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"18px"});
+			var turretCost2=$("<span/>").html("$1000").css({"height":"45px", "width":"45px","top":45, "position":"absolute", "left":"115px"});
+		var rightTurretControl=$("<div/>").css({"height":"63px", "width":"300px", "position":"absolute", "top":"10px","left":"840px", "background-color":"lightblue","border":"2px solid black","visibility":"hidden"});
 		var rightTurretButton1=$("<img>")
 			.attr({"src":"alienturret1.jpg"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"10px", "border":"1px solid black"})
@@ -211,17 +150,7 @@ function Control(game){
 			);
 		var rightTurretButton2=$("<img>")
 			.attr({"src":"alienturret2.jpg"})
-			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"})
-			.mouseover(
-				function(){
-					_this.rightGoldDisplay.html("Cost: $500");
-				}
-			)
-			.mouseout(
-				function(){
-					_this.rightGoldDisplay.html("");
-				}
-			);
+			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"110px", "border":"1px solid black"});
 		var exitButton=$("<img>")
 			.attr({"src":"X.png"})
 			.css({"height":"45px", "width":"45px", "position":"absolute", "left":"210px", "border":"1px solid black"})
@@ -232,7 +161,9 @@ function Control(game){
 				}
 			);
 		$(rightTurretControl).append(rightTurretButton1);
+		$(rightTurretControl).append(turretCost1);
 		$(rightTurretControl).append(rightTurretButton2);
+		$(rightTurretControl).append(turretCost2);
 		$(rightTurretControl).append(exitButton);
 		return rightTurretControl;
 	}
