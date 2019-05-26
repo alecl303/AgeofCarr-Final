a2f6dc71f57fc765aebfbaffff06c4089f16c767
andrewchung500 <50115267+andrewchung500@users.noreply.github.com>
2019-05-26 16:27:01 -0700

    Add files via upload

--- AlienBoss.js
+++ AlienBoss.js
@@ -3,7 +3,7 @@ function AlienBoss(game,side){
 	Character.apply(this,arguments);
     this.elem=document.createElement("canvas");
 	$(this.elem)
-		.css({position:'absolute',top:parseInt(game.elem.css('height'))-120,left:1000,backgroundColor:'green'})
+		.css({position:'absolute',top:parseInt(game.elem.css('height'))-120,left:1200})
 		.attr({height:160,width:1280/8});
 	this.game.elem.append(this.elem);
 	
@@ -16,16 +16,15 @@ function AlienBoss(game,side){
 	this.ctx = this.elem.getContext('2d');
 	this.ctx.translate(0,0);
 	this.ctx.beginPath();
-	
     var sheet=new Image();
     sheet.src="alienboss.png";
-    var fps = 10;
+     var fps = 20;
     var spritePosition=0;
-    var spriteWidth=1280/8;
-    var spriteHeight=160;
-    var spriteCount=8;
+     var spriteWidth=1280/8;
+     var spriteHeight=160;
+     var spriteCount=8;
         
-    this.health = 500;
+     this.health = 500;
 	this.xSpd = 10000;
 	this.range = -20;
 	this.dmg = 25;
@@ -63,8 +62,6 @@ function AlienBoss(game,side){
 
                 // Drawing code goes here
                 _this.ctx.clearRect(0,0,$(_this.elem).attr('width'),$(_this.elem).attr('height'));
-				_this.ctx.fillStyle = "blue";
-				_this.ctx.fillRect(0,0,$(_this.elem).attr('width'),$(_this.elem).attr('height'));
                 _this.ctx.drawImage(sheet,spritePosition*spriteWidth,0,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
 				//img, x coordinate where starting on spritesheet, sprite sheet y coordinate, width of clipped image, height, x, y , width, height
                 spritePosition++;
@@ -77,12 +74,12 @@ function AlienBoss(game,side){
         };
 
 	this.attack = function(){
-		$(_this.weapon.elem).css({left:parseInt($(_this.elem).css('left')), top:parseInt($(_this.elem).css('top'))+60,display:'block' })
+		$(_this.weapon.elem).css({left:parseInt($(_this.elem).css('left'))-20, top:parseInt($(_this.elem).css('top'))+60,display:'block' })
 		$(_this.weapon.elem)
 			.animate({opacity:0}
-			,340
+			,3400
 			,function(){
-				$(_this.weapon.elem).css({opacity:1});
+				$(_this.weapon.elem).css({opacity:100});
 					
 			}
 		);
